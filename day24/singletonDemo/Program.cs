e7f949cb465ac64556ede7d2a3ab74f4d6fe8cf0
harshalijadhav162 <jadhavharshali162@example.com>
2026-03-10 19:14:23 +0530

    Added Day24

---
+++ day24/singletonDemo/Program.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+var services = new ServiceCollection();
+services.AddSingleton<DiSingleton>();
+
+var serviceProvider = services.BuildServiceProvider();
+
+var diLogger1 = serviceProvider.GetService<DiSingleton>();
+var diLogger2 = serviceProvider.GetService<DiSingleton>();
+
+var loggerManual = new DiSingleton();
+
+Console.WriteLine(diLogger1.GetHashCode());
+Console.WriteLine(diLogger2.GetHashCode());
+Console.WriteLine(loggerManual.GetHashCode());
+
+public class DiSingleton
+{
+    public int Value { get; set; }
+}
